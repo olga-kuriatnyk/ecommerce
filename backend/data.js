ba2cb363a06042a52a79687a89c97461322aaa6f
olga-kuriatnyk <olhak@uw.edu>
2021-06-21 18:02:41 -0700

    create sample users in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Olga',
+      email: 'olha.kuriatnyk@gmail.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Alex',
+      email: 'user@gmail.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: '1',
