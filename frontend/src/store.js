788c0da35178a826c0a3f206abbe13a6c6d16ef2
olga-kuriatnyk <olhak@uw.edu>
2021-07-07 18:06:52 -0700

    implement update user profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,8 @@ import {
 import {
   userDetailsReducer,
   userRegisterReducer,
-  userSigninReducer
+  userSigninReducer,
+  userUpdateProfileReducer,
 } from './reducers/userReducer';
 
 const initialState = {
@@ -43,6 +44,7 @@ const reducer = combineReducers({
   orderPay: orderPayReducer,
   orderMineList: orderMineListReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
